535c2619cdc0bfabb6d275435361d1370e2b2d31
Redgreen64 <koltsc2014+@gmail.com>
2024-12-25 14:05:04 +0000

    Update installslatest.sh

--- installslatest.sh
+++ installslatest.sh
@@ -1,8 +1,9 @@
-echo off
 echo Redgreen's Scratch Installer
 sleep(1)
 echo Checking If Node JS Is Installed...
 sudo apt install nodejs -y
+echo Checking If Git Is Installed
+sudo apt install git -y
 echo Installed!
 sleep(1)
 git clone --depth=1 https://github.com/scratchfoundation/scratch-gui.git
